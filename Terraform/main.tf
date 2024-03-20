8446c23941c740ec8cd3e4dba88aaee1b505e2c0
AlonsoBTech <lonsobraithwaite1996@gmail.com>
2024-03-19 22:22:40 -0400

    First Commit

---
+++ Terraform/main.tf
@@ -0,0 +1,47 @@
+resource "aws_vpc" "GitHub_test" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "Git_VPC"
+  }
+}
+
+resource "aws_subnet" "Git_Public_Subnet_1" {
+  vpc_id                  = aws_vpc.project1.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ca-central-1a"
+
+  tags = {
+    Name = "Git_Public"
+  }
+}
+
+resource "aws_internet_gateway" "Git_IGW" {
+  vpc_id = aws_vpc.project1.id
+
+  tags = {
+    Name = "Git_IGW"
+  }
+}
+
+resource "aws_route_table" "Git_Public_Route" {
+  vpc_id = aws_vpc.project1.id
+
+  tags = {
+    Name = "Git_Pub_RT"
+  }
+}
+
+resource "aws_route" "Git_Route_Pub" {
+  route_table_id         = aws_route_table.Project_Public_Route.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.Project1_IGW.id
+}
+
+resource "aws_route_table_association" "Git_pub_asso1" {
+  subnet_id      = aws_subnet.Project1_Public_Subnet_1.id
+  route_table_id = aws_route_table.Project_Public_Route.id
+}
